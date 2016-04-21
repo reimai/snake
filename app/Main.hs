395a0796eda97838ef2258035db14b14bfd15da9
rei <naugadina@gmail.com>
2016-04-22 00:54:12 +0300

    treats are random; fixed rendering

--- app/Main.hs
+++ app/Main.hs
@@ -6,38 +6,45 @@ import Termin
 import Geom
 import Data.Maybe
 import Data.List
+import qualified System.Random as R 
 
 main :: IO()
 main = startGame action newWorld 
 
-newWorld :: World
-newWorld = World (newSnake (Crd 6 4) 14) False [] [meatball 10 10, meatball 20 30]
+newWorld :: GameState -> World
+newWorld game = World (newSnake (Crd 6 4) 16) [] $ genTreats game 30 
+
+genTreats :: GameState -> Int -> [Treat]
+genTreats (GameState (w,h) rnd) n = take n $ map (uncurry meatball) $ zip (R.randomRs (0, w) rnd_x) (R.randomRs (0,h) rnd_y)
+    where (rnd_x, rnd_y) = R.split rnd
+
 newSnake :: Crd -> Int -> Snake
-newSnake (Crd x y) len = Snake $ reverse [Crd xi y | xi <- [x..x+len] ]
+newSnake (Crd x y) len = Snake (reverse [Crd xi y | xi <- [x..x+len]]) False RightD
 
 meatball :: Int -> Int -> Treat
 meatball x y = Point (Crd x y) '*' 
 
-emptyTitle :: Title
-emptyTitle = Title "" $ Crd 0 0
+action :: World -> Char -> GameState -> World
+action world@(World snake@(Snake s dead prevDir) msg treats) ch game | dead = world
+                                                                     | otherwise = if (isDead nextSnake) 
+                                                                           then (World nextSnake [youDied (wnd game), debug snake] nextTreats) 
+                                                                           else World nextSnake [debug snake] nextTreats
+    where (nextSnake, nextTreats) = next snake game (getDir ch) treats   
 
-action :: World -> Char -> (Int,Int) -> World
-action world@(World snake@(Snake s) dead msg treats) ch (w, h) | dead = world
-                                                               | otherwise = if (isDead nextSnake) then (World nextSnake True [youDied (w, h)] nextTreats) else World nextSnake False [] nextTreats
-    where nextSnake = fst nextSnT
-          nextTreats = snd nextSnT
-          nextSnT = next snake dir treats   
-          dir = fromMaybe prevDir $ fmap (\d -> if (isOpposite d prevDir) then prevDir else d) (getDir ch)
-          prevDir = getPrevDir s         
-
-next :: Snake -> Dir -> [Treat] -> (Snake, [Treat]) 
-next (Snake body@(s:ss)) dir ts = (Snake $ newHead : (if (isJust ate) then body else init body), fromMaybe ts $ fmap (\i -> deleteByIndex i ts) ate)
-    where ate = elemIndex newHead (map crd ts)
-          newHead = move s dir 1  
+next :: Snake -> GameState -> Maybe Dir -> [Treat] -> (Snake, [Treat]) 
+next (Snake body@(s:ss) dead prevDir) (GameState wnd rnd) m_dir ts = (checkDead $ newSnake, fromMaybe ts $ fmap (\i -> deleteByIndex i ts) ate)
+    where newSnake = Snake (newHead : (if (isJust ate) then body else init body)) dead dir
+          dir = fromMaybe prevDir $ fmap (\d -> if (isOpposite d prevDir) then prevDir else d) m_dir 
+          ate = elemIndex newHead (map crd ts)
+          newHead = normalize wnd $ move s dir 1  
           
 deleteByIndex :: Int -> [a] -> [a]
 deleteByIndex i xs = fst splitted ++ (tail $ snd splitted) 
     where splitted = splitAt i xs
+
+debug :: Show a => a -> Title
+--debug obj = Title (show obj) $ Crd 1 1
+debug obj = Title "" $ Crd 1 1
 
 getDir :: Char -> Maybe Dir
 getDir 'D' = Just LeftD
@@ -46,30 +53,25 @@ getDir 'C' = Just RightD
 getDir 'B' = Just DownD
 getDir _   = Nothing
 
-getPrevDir :: [Crd] -> Dir
-getPrevDir s@((Crd ax ay):(Crd bx by):ts) | ax < bx = LeftD
-                                          | ax > bx = RightD
-                                          | ay < by = UpD
-                                          | ay > by = DownD
-                                          | otherwise = error $ "s: "++ show s
-
-isDead :: Snake -> Bool
-isDead (Snake (s:ss)) = s `elem` ss
+checkDead :: Snake -> Snake
+checkDead (Snake (s:ss) dead prevDir) = Snake (s:ss) (dead || died) prevDir
+    where died = s `elem` ss
 
 youDied :: (Int, Int) -> Title
 youDied = center "YOU DIED" 
 
-data Snake = Snake {body :: [Crd]}
+data Snake = Snake {body :: [Crd], isDead :: Bool, prevDir :: Dir}
     deriving (Eq, Show)
 
 type Treat = Point
 
 instance Renderable Snake where
-    render (Snake s_body) = Point (head s_body) '▒' : map (\c -> Point c '█') (init.tail $ s_body) ++ [Point (last s_body) '█']
+    render (Snake s_body dead _) = Point (head s_body) head_sym : map (\c -> Point c '█') (init.tail $ s_body) ++ [Point (last s_body) '█']
+        where head_sym = if dead then 'X' else '▒'
 
-data World = World {snake :: Snake, dead :: Bool, msg :: [Title], treats :: [Treat]}
+data World = World {snake :: Snake, msg :: [Title], treats :: [Treat]}
 instance Renderable World where
-    render (World snake dead msg treats) = concat $ map render (RS snake:(map RS msg) ++ (map RS treats)) --great type system, they said 
+    render (World snake msg treats) = concat $ map render (RS snake:(map RS treats) ++ (map RS msg)) --great type system, they said 
 
 data Renderables = forall a. Renderable a => RS a
 instance Renderable Renderables where
--- src/Termin.hs
+++ src/Termin.hs
@@ -1,5 +1,5 @@
 module Termin (
-    addToScreen, startGame, readAll 
+    addToScreen, readAll, startGame, normalize, GameState(..) 
 ) where
 
 import Data.List
@@ -16,54 +16,65 @@ import System.IO
 import Control.Concurrent
 import System.CPUTime
 import Geom
+import System.Random
+
+data GameState = GameState{wnd :: (Int, Int), rnd :: StdGen}
+
+
+startGame :: Renderable a => (a -> Char -> GameState -> a) -> (GameState -> a) -> IO()                      
+startGame action init_world = do
+        hSetBuffering stdin NoBuffering --get input immedietly
+        hSetEcho stdin False            --don't show the typed character
+        jwindow <- T.size
+        let wnd = fromJust jwindow
+        rnd <- getStdGen
+        let w = T.width wnd
+        let h = T.height wnd - 1
+        let (init_rnd, main_rnd) = split rnd
+        let game = GameState (w, h) main_rnd
+        let world = init_world $ GameState (w, h) init_rnd 
+        gameLoop stdin game world action
+
+
+--main loop, read input, change the world, redraw screen 
+gameLoop :: Renderable a => Handle -> GameState -> a -> (a -> Char -> GameState -> a) -> IO()
+gameLoop input game@(GameState (w, h) rnd) world action = do 
+                        mapM_ putStrLn $ addToScreen (replicate h $ replicate w ' ') (w,h) world 
+                        e <- threadDelay (floor(1/fps * 10^6))  
+                        ch <- readAll input ' '
+                        when (ch /= 'q') $ gameLoop input game (action world ch game) action 
+                                where fps = 15
 
 
 addToScreen :: Renderable a => [String] -> (Int, Int) -> a -> [String]
-addToScreen ls size obj = map (\(i, l, ps) -> mergeLine l ps) $ groupByLines ls $ normalize (render obj) size 
+addToScreen ls wnd obj = map (\(i, l, ps) -> mergeLine l ps) $ groupByLines ls $ filter (\p -> fits wnd $ crd p) $ render obj  
 
-normalize :: [Point] -> (Int, Int) -> [Point]
-normalize ps (w, h) = map (\(Point (Crd x y) s) -> Point (Crd (x `mod` w) (y `mod` h)) s) ps 
+fits :: (Int,Int) -> Crd -> Bool
+fits (w,h) (Crd x y) = x < w && x >= 0 && y >= 0 && y < h                             
+
+normalize :: (Int, Int) -> Crd -> Crd
+normalize (w, h) (Crd x y) = Crd (x `mod` w) (y `mod` h) 
 
 mergeLine :: String -> [Point] -> String
 mergeLine line ps = mergeLine' (zip [0..] line) $ sortOn (x.crd) ps 
 
 mergeLine' :: [(Int, Char)] -> [Point] -> String
 mergeLine' l [] = map snd l 
-mergeLine' [] p = error ""
-mergeLine' (l:ls) (p:ps) | (fst l) < (x.crd $ p) = (snd l):(mergeLine' ls (p:ps))
-                         | otherwise             = (sym p):(mergeLine' ls ps)
+mergeLine' [] p = error $ "no cols " ++ (show p)
+mergeLine' lines@((il, l):ls) (p:ps) | il <  (x.crd $ p) = l:(mergeLine' ls (p:ps))
+                               | il == (x.crd $ p) = (sym p):(mergeLine' ls ps)
+                               | otherwise         = mergeLine' lines ps --duplicate crd
 
 
 groupByLines  :: [String] -> [Point] -> [(Int, String, [Point])]
 groupByLines ls ps = mergeToLines (zip3 [0..] ls $ repeat []) $ map (\p -> (y.crd $ p, p)) $ sortOn (y.crd) ps
 
-mergeToLines :: Ord a => [(a, b, [c])] -> [(a, c)] -> [(a, b, [c])]
+mergeToLines :: (Ord a, Show a, Show c) => [(a, b, [c])] -> [(a, c)] -> [(a, b, [c])]
 mergeToLines x [] = x
-mergeToLines [] y = error "no lines"
+mergeToLines [] y = error $ "no lines" ++ (show y)
 mergeToLines ((x, xd, xyd):xs) ((y, yd):ys) | x < y     = (x, xd, xyd):(mergeToLines xs ((y, yd):ys))
                                             | otherwise = mergeToLines ((x, xd, yd:xyd):xs) ys 
                
-startGame :: Renderable a => (a -> Char -> (Int, Int) -> a) -> a -> IO()                      
-startGame action world = do
-        hSetBuffering stdin NoBuffering --get input immedietly
-        hSetEcho stdin False            --don't show the typed character
-        jwindow <- T.size
-        let wnd = fromJust jwindow
-        gameLoop stdin wnd world action
-
---main loop, read input, change the world, redraw screen 
-gameLoop :: Renderable a => Handle -> Window Int -> a -> (a -> Char -> (Int, Int) -> a) -> IO()
-gameLoop h wnd world action = do 
-                        --clearScreen
-                        mapM_ putStrLn $ addToScreen (replicate height $ replicate width ' ') (width, height) world 
-                        e <- threadDelay (floor(1/fps * 10^6))  
-                        ch <- readAll h ' '
-                        when (ch /= 'q') $ gameLoop h wnd (action world ch (width, height)) action 
-                                where fps      = 15
-                                      height   = T.height wnd - 2
-                                      width    = T.width wnd
-
 --read all input from Handle, return only the last char or default
 readAll :: Handle -> Char -> IO(Char)
 readAll h defaultCh = hReady h >>= \gotIt -> if gotIt then (hGetChar h >>= readAll h) else return defaultCh
-
